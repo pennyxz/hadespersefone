9dce0202af2bff82a1dd98297a9141a83d4106db
jinmyjbv <jinmybohorquez@gmail.com>
2016-01-08 00:24:11 -0430

    Actualizacion exportBtn

--- vulcano/Schematic Builder/jbScript.js
+++ vulcano/Schematic Builder/jbScript.js
@@ -1,21 +1,24 @@
+var contadorEquipos = 0;
+
 $(document).ready(function(){
+	
 	//alert("doc ready");
 	var isCollectionACharged = false, isCollectionBCharged = false,
 		isCollectionCCharged = false;
 	var collectionA =[
-		['<li class="collectionA" onclick="clickFunction(this)">0</li>', 1, 1],
-		['<li class="collectionA" onclick="clickFunction(this)">1</li>', 1, 1],
-		['<li class="collectionA" onclick="clickFunction(this)">2</li>', 1, 1]
+		['<li class="collectionA" onclick="clickFunction(this)">bombaA</li>', 1, 1],
+		['<li class="collectionA" onclick="clickFunction(this)">bombaB</li>', 1, 1],
+		['<li class="collectionA" onclick="clickFunction(this)">bombaC</li>', 1, 1]
 	];
 	var collectionB =[
-		['<li class="collectionB" onclick="clickFunction(this)">3</li>', 1, 1],
-		['<li class="collectionB" onclick="clickFunction(this)">4</li>', 1, 1],
-		['<li class="collectionB" onclick="clickFunction(this)">5</li>', 1, 1]
+		['<li class="collectionB" onclick="clickFunction(this)">equipoA</li>', 1, 1],
+		['<li class="collectionB" onclick="clickFunction(this)">equipoB</li>', 1, 1],
+		['<li class="collectionB" onclick="clickFunction(this)">equipoC</li>', 1, 1]
 	];
 	var collectionC =[
-		['<li class="collectionC" onclick="clickFunction(this)">6</li>', 1, 1],
-		['<li class="collectionC" onclick="clickFunction(this)">7</li>', 1, 1],
-		['<li class="collectionC" onclick="clickFunction(this)">8</li>', 1, 1]
+		['<li class="collectionC" onclick="clickFunction(this)">aireA</li>', 1, 1],
+		['<li class="collectionC" onclick="clickFunction(this)">aireB</li>', 1, 1],
+		['<li class="collectionC" onclick="clickFunction(this)">aireC</li>', 1, 1]
 	];
 	/* <li data-row="1" data-col="1" data-sizex="1" data-sizey="1" class="gs-w"><span class="gs-resize-handle gs-resize-handle-both"></span></li>
        <li data-row="2" data-col="1" data-sizex="1" data-sizey="1" class="gs-w"><span class="gs-resize-handle gs-resize-handle-both"></span></li>
@@ -65,7 +68,19 @@ $(document).ready(function(){
 		
 	});
 	
-	
+	//modal wait until the DOM has loaded before querying the document
+	$.get('ajax.html', function(data){
+		modal.open({content: data});
+	});
+
+	$('#btnExportar').click(function(e){
+		var codigo = $('#esquema').html();
+		//alert(codigo);
+		modal.open({content: "<div>HTML</div>"
+					+codigo});
+		
+		e.preventDefault();
+	});
 	
 });
 
@@ -78,8 +93,75 @@ function clickFunction(liObject){
 	//alert(x);
 	esquema.add_widget($(liObject).prop('outerHTML'));
 	$('#esquema ul li:last').removeAttr("onclick").css("display", "");
+	
 
 }
+//modal export screen
+var modal = (function(){
+				var 
+				method = {},
+				$overlay,
+				$modal,
+				$content,
+				$close;
+
+				// Center the modal in the viewport
+				method.center = function () {
+					var top, left;
+
+					top = Math.max($(window).height() - $modal.outerHeight(), 0) / 2;
+					left = Math.max($(window).width() - $modal.outerWidth(), 0) / 2;
+
+					$modal.css({
+						top:top + $(window).scrollTop(), 
+						left:left + $(window).scrollLeft()
+					});
+				};
+
+				// Open the modal
+				method.open = function (settings) {
+					$content.empty().append(settings.content);
+
+					$modal.css({
+						width: settings.width || 'auto', 
+						height: settings.height || 'auto'
+					});
+
+					method.center();
+					$(window).bind('resize.modal', method.center);
+					$modal.show();
+					$overlay.show();
+				};
+
+				// Close the modal
+				method.close = function () {
+					$modal.hide();
+					$overlay.hide();
+					$content.empty();
+					$(window).unbind('resize.modal');
+				};
+
+				// Generate the HTML and add it to the document
+				$overlay = $('<div id="overlay"></div>');
+				$modal = $('<div id="modal"></div>');
+				$content = $('<div id="content"></div>');
+				$close = $('<a id="close" href="#">close</a>');
+
+				$modal.hide();
+				$overlay.hide();
+				$modal.append($content, $close);
+
+				$(document).ready(function(){
+					$('body').append($overlay, $modal);						
+				});
+
+				$close.click(function(e){
+					e.preventDefault();
+					method.close();
+				});
+
+				return method;
+			}());
 
 
 
